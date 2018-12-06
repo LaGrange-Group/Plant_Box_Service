0907c9bb792b939ca32c0497d76b60b9c3a77f4a
Lagrango <davidnlagrange@hotmail.com>
2018-12-06 09:12:35 -0600

    Added Concept Documentation

---
+++ Plant_Box_Service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Plant_Box_Service.MvcApplication" Language="C#" %>
---
+++ Plant_Box_Service/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Plant_Box_Service.Startup))]
+namespace Plant_Box_Service
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
