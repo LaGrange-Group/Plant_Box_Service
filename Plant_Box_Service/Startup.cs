60f944421050a3ad9b3c0630007e55c7188bd5c4
Lagrango <davidnlagrange@hotmail.com>
2018-12-06 10:05:09 -0600

    Added Roles and Login Functionality

--- Plant_Box_Service/Startup.cs
+++ Plant_Box_Service/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using Plant_Box_Service.Models;
 
 [assembly: OwinStartupAttribute(typeof(Plant_Box_Service.Startup))]
 namespace Plant_Box_Service
@@ -9,6 +12,15 @@ namespace Plant_Box_Service
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            ApplicationDbContext db = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+            }
         }
     }
 }
